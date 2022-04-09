cac9f13173d4491babc04a0ee8f82725cbd3ffab
storezhang <storezhang@gmail.com>
2022-04-09 01:21:14 +0000

    feat(项目迁移): 从gox迁移到paging

---
+++ page_option.go
@@ -0,0 +1,20 @@
+package paging
+
+type (
+	pageOption interface {
+		applyPage(options *pageOptions)
+	}
+
+	pageOptions struct {
+		size   int32
+		page   int32
+		fields []Field
+	}
+)
+
+func defaultPageOptions() *pageOptions {
+	return &pageOptions{
+		size: 20,
+		page: 1,
+	}
+}
